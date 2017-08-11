f90a0dfcdf7e4791cbc28229aaf25054f790aa76
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-08-11 19:47:17 -0300

    Allow create and delete phones though contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,14 +2,14 @@ class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
 
-  accepts_nested_attributes_for :phones
+  accepts_nested_attributes_for :phones, allow_destroy: true
 
   def kind_description
     kind.description
   end
 
   def as_json(options={})
-    hash = super(methods: :kind_description, include: {kind: {except: :description}})
+    hash = super(methods: :kind_description, include: [:kind, :phones])
     hash[:birthdate] = I18n.l(self.birthdate) if birthdate.present?
     hash
   end
