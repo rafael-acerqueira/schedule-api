daff2ae931a27afbecd56685c9801c5a345a8862
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-08-21 16:29:26 -0300

    Set json API adapter

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthdate
+
+  def attributes(*args)
+    hash = super(*args)
+    # pt-BR --> hash[:birthdate] = I18n.l(object.birthdate) if object.birthdate.present?
+    hash[:birthdate] = object.birthdate.to_time.iso8601 if object.birthdate.present?
+    hash
+  end
 end
