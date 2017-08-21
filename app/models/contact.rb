d97a091adc3be69fb42e800dfceb86d60b663cd2
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-08-21 16:15:54 -0300

    Setup active model serializer gem

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,13 +6,13 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address
 
-  def kind_description
-    kind.description
-  end
-
-  def as_json(options={})
-    hash = super(methods: :kind_description, include: [:kind, :phones, :address])
-    hash[:birthdate] = I18n.l(self.birthdate) if birthdate.present?
-    hash
-  end
+  # def kind_description
+  #   kind.description
+  # end
+  #
+  # def as_json(options={})
+  #   hash = super(methods: :kind_description, include: [:kind, :phones, :address])
+  #   hash[:birthdate] = I18n.l(self.birthdate) if birthdate.present?
+  #   hash
+  # end
 end
