8f3a85690e573ee8fe67caf3b26cc3cf66a0445d
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-08-21 17:51:14 -0300

    Add HATEOAS links

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related){kind_url(object.kind.id)}
+  end
   has_many :phones
   has_many :address
+
+  link(:self){contact_url(object.id)}
+
 
   def attributes(*args)
     hash = super(*args)
