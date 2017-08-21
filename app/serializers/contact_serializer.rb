fcf174c9cf4ee1018103abbf6b44ff937ead1dd2
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-08-21 16:47:14 -0300

    Add contact associations

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones
+  has_many :address
+
   def attributes(*args)
     hash = super(*args)
     # pt-BR --> hash[:birthdate] = I18n.l(object.birthdate) if object.birthdate.present?
