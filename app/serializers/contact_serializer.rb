c9bbbb8fe4947d6a7dc0fe4537879b40d10c741b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-08-24 20:51:47 -0300

    Adjust routes and has_one relationship to cope with json API

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -9,7 +9,9 @@ class ContactSerializer < ActiveModel::Serializer
     link(:related){contact_phones_url(object.id)}
   end
 
-  has_many :address
+  has_one :address do
+    link(:related){contact_address_url(object.id)}
+  end
 
   link(:self){contact_url(object.id)}
 
